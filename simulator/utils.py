7a9278764947311405ccf8378e42c80e9b3ef567
Simone Rossi Tisbeni <simone.rossi22@studio.unibo.it>
2019-09-27 11:21:38 +0200

    Added main function and arg parser

---
+++ simulator/utils.py
@@ -0,0 +1,13 @@
+import argparse
+
+PROGRAM_VERSION = 'v0.1'
+
+
+class OptionParser():
+    def __init__(self, program_name='PROG'):
+        """User based option parser"""
+        self.parser = argparse.ArgumentParser(program_name)
+        self.parser.add_argument("--debug", "-d", action='store_true', dest='debug',
+                                 default=False, help='show verbose output for debugging')
+        self.parser.add_argument('--version', action='version',
+                                 version=PROGRAM_VERSION)
