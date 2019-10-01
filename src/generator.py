e5c628b5241dcb5b368cb39c8cedceb0d54278c7
Simone Rossi Tisbeni <simone.rossi22@studio.unibo.it>
2019-10-01 13:32:24 +0200

    First draft of template generation

--- src/generator.py
+++ src/generator.py
@@ -1,24 +1,56 @@
+import random
 from .utils import read_config
 
 
 class Template():
     def __init__(self):
-        self.template = ['descriptor', 'descriptor', 'PARAM', ]
+        ran = random.randint(3, 7)
+        lista = []
+        for i in range(ran):
+            dp = random.randint(0, 1)
+            if dp == 0:
+                lista.append(f'descriptor{i}')
+            else:
+                lista.append(f'PARAM')
+        self.template = lista
+
+    def write_line(self):
+        string = ""
+        for i, t in enumerate(self.template):
+            if t == 'PARAM':
+                string = string + f'PARAM{i}' + ' '
+            else:
+                string = string + t + ' '
+        return string.rstrip()
+
+
+def generate_templates(params):
+    n = params['templates']
+    templates = []
+    for i in range(n):
+        t = Template()
+        templates.append(t)
+    return templates
+
+
+def select_template(template_list):
+    n = len(template_list)
+    rnd = random.randint(0, n-1)
+    return template_list[rnd]
 
 
 def generate_log():
     """Generate Log Files based on configuration"""
     config_filename = 'config.json'
     config_dict = read_config(config_filename)
+
     filename = config_dict['filename']
     lines = config_dict['lines']
-    log_line = Template()
+
+    templates = generate_templates(config_dict)
+
     with open(filename, 'w') as f:
         for i in range(lines):
-            line = ""
-            for element in log_line.template:
-                if element == 'PARAM':
-                    line = line + f'PARAM{i} '
-                else:
-                    line = line + element + ' '
-            f.write(line + '\n')
+            t = select_template(templates)
+            s = t.write_line()
+            f.write(s + '\n')
